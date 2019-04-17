3d4410ccdc59a27038584ef0d28eafa00ff23217
Gabe Velasquez <gvelasquez@gmail.com>
2019-04-17 16:54:21 -0700

    first commit

---
+++ apps/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+from time import gmtime, strftime
+
+# Create your views here.
+def index(request):
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+    }
+    return render(request, "main/index.html", context)
